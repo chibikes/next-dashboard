699753919bf3ae37cd99bf2e5ff642a2d5fd1696
chibikes <okoh_chibuike@yahoo.com>
2024-06-07 14:29:50 +0100

    configure authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import '@/app/ui/global.css';
 import {inter} from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+// this automatically adds this metadata automatically to your app
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
